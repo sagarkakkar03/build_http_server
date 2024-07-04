b010a4136e888b2a6780aa2899589489308245dc
Sagar <kakkarsagar03.com>
2024-07-04 11:39:41 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,10 +8,17 @@ def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, client_add = server_socket.accept() # wait for client
-    client_socket.recv(4096)
-
+    # print('p')
+    data = client_socket.recv(4096).decode()
+    lst = list(data.split('\r\n'))
+    # print(lst)
     http_response = 'HTTP/1.1 200 OK\r\n\r\n'
-    client_socket.sendall(http_response.encode('ASCII'))
+    # print(lst[0].split(' '))
+    if lst[0].split(' ')[1] == '/':
+        client_socket.sendall(http_response.encode('ASCII'))
+    else:
+        client_socket.sendall('HTTP/1.1 404 Not Found\r\n\r\n'.encode('ASCII'))
     client_socket.close()
+    
 if __name__ == "__main__":
     main()
